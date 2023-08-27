14cff3a1744da97e4cd7bc96520dd1e1197e8fa5
piwiit <bouillaux@hotmail.fr>
2023-08-27 18:25:51 +0200

    add class

---
+++ src/modules/Task.js
@@ -0,0 +1,6 @@
+export class Task {
+  constructor(title, dueDate = 'no date') {
+    this.title = title;
+    this.dueDate = dueDate;
+  }
+}
---
+++ src/modules/TaskList.js
@@ -0,0 +1,5 @@
+export class TaskList {
+  constructor() {
+    this.tasks = [];
+  }
+}
