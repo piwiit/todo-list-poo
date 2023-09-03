c793b99a2d452f326b64a614966bf535ac314517
piwiit <bouillaux@hotmail.fr>
2023-09-03 18:01:10 +0200

    add function in tasklist constructor

--- src/modules/TaskList.js
+++ src/modules/TaskList.js
@@ -2,4 +2,15 @@ export class TaskList {
   constructor() {
     this.tasks = [];
   }
+
+  addTask(task) {
+    this.tasks.push(this.task);
+  }
+
+  removeTask(task) {
+    const index = this.tasks.indexOf(task);
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+    }
+  }
 }
